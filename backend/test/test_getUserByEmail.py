e8ec89981acf19e51cb1352ecb4dd4fc2070bd3b
Patrik Arvius <patrik.arvius@gmail.com>
2025-05-08 11:18:49 +0200

    Split test cases to ensure single assertions

--- backend/test/test_getUserByEmail.py
+++ backend/test/test_getUserByEmail.py
@@ -8,14 +8,21 @@ def user_controller():
     return UserController(dao=mock_dao)
 
 @pytest.mark.getuserbyemail
-def test_valid_email_matches_zero_users_prints_warning_returns_none(user_controller, capsys):
+def test_returns_none_when_zero_users_found(user_controller):
     user_controller.dao.find.return_value = []
 
     result = user_controller.get_user_by_email("nonexistent@example.com")
+
+    assert result is None
+
+@pytest.mark.getuserbyemail
+def test_prints_warning_when_zero_users_found(user_controller, capsys):
+    user_controller.dao.find.return_value = []
+
+    user_controller.get_user_by_email("nonexistent@example.com")
     captured = capsys.readouterr()
 
     assert "Warning: no user found with mail nonexistent@example.com" in captured.out
-    assert result is None
 
 @pytest.mark.getuserbyemail
 def test_valid_email_matches_one_user_returns_userobject(user_controller):
@@ -27,19 +34,27 @@ def test_valid_email_matches_one_user_returns_userobject(user_controller):
     assert result == mocked_user
 
 @pytest.mark.getuserbyemail
-def test_prints_warning_when_multiple_users_found_returns_first_user(user_controller, capsys):
+def test_returns_first_user_when_multiple_users_found(user_controller):
     mocked_user1 = {"email": "existingmultiple@email.com", "id": "1"}
     mocked_user2 = {"email": "existingmultiple@email.com", "id": "2"}
-    user_controller.dao.find.return_value = [
-        mocked_user1,
-        mocked_user2,
-    ]
+    user_controller.dao.find.return_value = [mocked_user1, mocked_user2]
 
     result = user_controller.get_user_by_email("existingmultiple@email.com")
-    captured = capsys.readouterr()
 
     assert result == mocked_user1
+
+
+@pytest.mark.getuserbyemail
+def test_prints_warning_when_multiple_users_found(user_controller, capsys):
+    mocked_user1 = {"email": "existingmultiple@email.com", "id": "1"}
+    mocked_user2 = {"email": "existingmultiple@email.com", "id": "2"}
+    user_controller.dao.find.return_value = [mocked_user1, mocked_user2]
+
+    user_controller.get_user_by_email("existingmultiple@email.com")
+    captured = capsys.readouterr()
+
     assert "Error: more than one user found with mail existingmultiple@email.com" in captured.out
+
 
 @pytest.mark.getuserbyemail
 def test_valid_email_db_operation_fails_raises_exception(user_controller):
